bd176ee3ad368d8ab4917ac091095dd52413781a
Usuario <Usuario@10.230.170.204>
2017-11-17 11:27:35 +0000

    Mostramos Registros de una BBDD de sql-Lite Creamos una tabla llamada emails para almacenar los emails de un archivo txt.

--- src/kata5p1/Kata5P1.java
+++ src/kata5p1/Kata5P1.java
@@ -29,5 +29,30 @@ public class Kata5P1 {
             String departamento = rs.getString("Departamento");
             System.out.println(Id + "\t" + name + "\t" + apellidos + "\t" + departamento);
         }
+        stmt.executeUpdate("drop table if exists emails");
+        stmt.executeUpdate("create table emails (Id integer primary key autoincrement, Mail string not null)");
+        
+        String fileName = "D:/Usuario/Desktop/DAVID/Universidad/IS2/Practica Lab 5/emails.txt";
+        BufferedReader reader = new BufferedReader(new FileReader(new File(fileName)));
+        String mail="";
+        
+        while(((mail = reader.readLine()) != null)) {
+            if(!mail.contains("@")) continue;
+            stmt.executeUpdate("insert into emails (Mail) values('"+mail+"')");   
+        }
+        reader.close();
+        
+        
+        
     }
 }
+
+/*
+statement.setQueryTimeout(30);  // set timeout to 30 sec.
+
+      statement.executeUpdate("drop table if exists person");
+      statement.executeUpdate("create table person (id integer, name string)");
+      statement.executeUpdate("insert into person values(1, 'leo')");
+      statement.executeUpdate("insert into person values(2, 'yui')");
+      ResultSet rs = statement.executeQuery("select * from person");
+*/
